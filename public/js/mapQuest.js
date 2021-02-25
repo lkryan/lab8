5ed01d1582a8061834ec17fc15567fef71807269
Lucas Ryan <lkryan@ucsd.edu>
2021-02-25 11:21:47 -0800

    thirdparty API lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = '1kgHXWKbj2xlQevBexVCuY5pvn0co7aV';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.878587858374, -117.23589673114331],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 14,
+  zoomControl: false
+});
+
+L.marker([32.878587858374, -117.23589673114331]).addTo(map);
+
 }
